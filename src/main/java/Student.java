594b76f86f4c419de7c0a3d8816819bcae1d67c0
Andrew Lange <andrew.lange13@gmail.com>
2022-03-14 14:14:23 -0500

    adds getters and setters

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -12,6 +12,27 @@ public class Student {
         this.grades = new ArrayList<>();
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getGradeAverage(){
+        double sum = 0;
+        for (Integer grade : grades){
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
 }
