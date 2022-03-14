5879d2ee7148392dc214c52e50083f80147b48c8
Andrew Lange <andrew.lange13@gmail.com>
2022-03-14 15:06:33 -0500

    adds first set of student tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -20,6 +19,23 @@ public class StudentTest {
         assertEquals(46, bob.getId());
         assertEquals("bob", bob.getName());
         assertTrue(bob.getGrades() instanceof ArrayList<Integer>);
+    }
+
+    @Test
+    public void addGradeTest(){
+        Student bob = new Student("bob", 13);
+        bob.addGrade(76);
+        bob.addGrade(78);
+        assertSame(76, bob.getGrades().get(0));
+        assertSame(78, bob.getGrades().get(1));
+    }
+
+    @Test
+    public void averageTest(){
+        Student bob = new Student("bob", 13);
+        bob.addGrade(76);
+        bob.addGrade(78);
+        assertEquals(77, bob.getGradeAverage(), 0);
     }
 
 }
