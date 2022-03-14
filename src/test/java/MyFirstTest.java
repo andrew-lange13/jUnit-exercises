8b22580c2da2fa1dd0e3ec5716bfd254c1c07c94
Andrew Lange <andrew.lange13@gmail.com>
2022-03-14 13:57:23 -0500

    adds tests to myFirstTest

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -20,4 +20,26 @@ public class MyFirstTest {
 
     }
 
+    @Test
+    public void numbersTest(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+        assertArrayEquals(numbers, otherNumbers);
+
+    }
+
+    @Test
+    public void phpTest(){
+        String language = "PHP";
+        language.contains("H"); // use assertTrue for this statement
+        language.contains("J"); // use assertFalse for this statement
+        assertTrue(language, true);
+        assertFalse(language, false);
+
+    }
+
+
 }
